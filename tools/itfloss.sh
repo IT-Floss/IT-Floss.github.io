318356b9fedf1fb1218fdeb29bf1f27c264c2549
SebaMinguez <sebaminguez@gmail.com>
2024-02-05 22:22:19 -0300

    quick GH actions fix

--- tools/itfloss.sh
+++ tools/itfloss.sh
@@ -16,7 +16,7 @@ create_adminbirras(){
   [[ -z "${NOMBRE_ARCHIVO}" ]] && NOMBRE_ARCHIVO=$(date +%B)
 
   # Crear adminbirras segun el tipo de archivo definido en archetype/adminbirras.md
-  EVENT_FILE_PATH=$(hugo new --kind adminbirras eventos/adminbirras/${NOMBRE_ARCHIVO}.md | awk '{print $1}')
+  EVENT_FILE_PATH=$(hugo new --kind adminbirras eventos/adminbirras/${NOMBRE_ARCHIVO}.md | awk '{print $2}' | tr -d '"')
 
   if [[ -f "${EVENT_FILE_PATH}" ]]; then
     # Asignar paramentros  
